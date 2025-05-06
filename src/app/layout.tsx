2e9f9f24e8d3df7d36ea077042c94b30f12914a9
ljonespwc <lancecj@gmail.com>
2025-05-05 21:56:39 -0700

    one more lint error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { cookies } from 'next/headers';
+
 import "./globals.css";
 
 const geistSans = Geist({
